9f1ccc68f623c3fe4c20f2e8daec6e51d81ef786
Franz <franzns11@gmail.com>
2026-09-25 14:49:34 +0200

    cadence for cg

--- config/worker-jobs.ts
+++ config/worker-jobs.ts
@@ -38,7 +38,7 @@ export const activeChainWorkerJobsGeneric: WorkerJob[] = [
 
 // Jobs that are not chain specific. Attached to the single configured chain.
 export const activeChainWorkerJobsGlobal: WorkerJob[] = [
-    { name: 'update-token-prices', interval: interval(every(15, 'minutes'), every(5, 'minutes')) },
+    { name: 'update-token-prices', interval: every(15, 'minutes') },
     { name: 'global-purge-old-data', interval: every(1, 'days') },
     { name: 'sync-rate-provider-reviews', interval: interval(every(30, 'minutes'), every(15, 'minutes')) },
     { name: 'sync-hook-reviews', interval: interval(every(30, 'minutes'), every(15, 'minutes')) },
